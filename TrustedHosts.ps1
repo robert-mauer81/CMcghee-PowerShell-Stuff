a7359fe6db87a1af0133cd79edb6633478b458cf
Christian McGhee <chrs.mcghee@gmail.com>
2021-09-07 11:23:24 -0500

    Add files via upload

---
+++ TrustedHosts.ps1
@@ -0,0 +1,35 @@
+﻿#Check PS version
+
+$PSVersionTable
+
+
+#Modify local TrustedHosts
+
+
+
+Set-Item WSMAN:\localhost\Client\TrustedHosts -Value NYC-SRV2
+
+Get-Item WSMAN:\localhost\Client\TrustedHosts
+
+Clear-Item WSMAN:\localhost\Client\TrustedHosts
+
+#or Using winrm from elevated command prompt
+
+winrm set winrm/config/client @{TrustedHosts="NYC-SRV2"}
+
+
+Enter-PSSession -ComputerName NYC-SRV2 -credential contoso\admin1
+Exit-PSSession
+
+#Alternativly
+
+$session = New-PSSession -ComputerName NYC-SRV2 -Credential contoso\Admin1
+
+Get-PSSession
+
+Enter-PSSession -Id <ID#>
+
+Get-PSSession | Remove-PSSession
+
+#or
+Invoke-command -session $session -ScriptBlock{Get-EventLog -LogName system -Newest 3}
