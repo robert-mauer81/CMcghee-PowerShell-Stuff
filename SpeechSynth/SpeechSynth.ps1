950b796343f633939310a878ddae2851b1ba546d
Christian McGhee <chrs.mcghee@gmail.com>
2021-11-12 11:45:10 -0600

    Speech Synthesizer

---
+++ SpeechSynth/SpeechSynth.ps1
@@ -0,0 +1,28 @@
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function new-speech {
+    Param
+    (
+        [Parameter(Mandatory = $true, 
+            ValueFromPipeline = $true)]
+        [string]$text
+    )
+
+    #set up .net object for use
+    Add-Type -AssemblyName System.Speech 
+    $synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+    
+    Write-Host $text
+    $synth.speak($text)
+ 
+}
+
+new-speech -text "This is a test"
