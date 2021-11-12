764107a86986aa51377ac86fd756f9cc3d57e4b2
Christian McGhee <chrs.mcghee@gmail.com>
2021-11-12 11:04:34 -0600

    Added Comment

--- RenameFiles.ps1
+++ RenameFiles.ps1
@@ -1,4 +1,5 @@
 #create list of files in array and rename txt to ps1
+#Consider adding variable for A and B
 
 $files = Get-ChildItem -Path E:\mod08\democode\ -Exclude computers.txt -ErrorAction SilentlyContinue
 foreach ($file in $files) {
