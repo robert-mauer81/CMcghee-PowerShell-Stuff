693cf2b896d7ba605a66ec83cefe1a11453e814e
Christian McGhee <chrs.mcghee@gmail.com>
2021-11-12 10:45:12 -0600

    Test1

---
+++ RenameFiles.ps1
@@ -0,0 +1,8 @@
+#create list of files in array and rename txt to ps1
+
+$files = Get-ChildItem -Path E:\mod08\democode\ -Exclude computers.txt -ErrorAction SilentlyContinue
+foreach ($file in $files) {
+    $newfile = $file.Name.Replace("txt", "ps1")  #replace "A" with "B"
+    
+    Rename-Item $file $newFile
+}
