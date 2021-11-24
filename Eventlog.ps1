d48bff48e256b45989d2f3f5d921c00f3d4bb1c6
Christian McGhee <chrs.mcghee@gmail.com>
2021-11-24 16:14:12 -0600

    This is still a banana

--- Eventlog.ps1
+++ Eventlog.ps1
@@ -1,3 +1,3 @@
-$logname = (Read-host -Prompt "Entr Logname")
-$Number = (Read-host -Prompt "Enter Number")
-Get-eventlog -logname $logname -Newest $number
+$logname = (Read-host -Prompt "Enter Logname")
+$newnumber = (Read-host -Prompt "Enter Number")
+Get-eventlog -logname $logname -Newest $newnumber
