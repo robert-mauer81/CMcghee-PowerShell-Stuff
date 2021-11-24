637f96adc54eb52f30beb295606d2c9a0ae307d2
Christian McGhee <chrs.mcghee@gmail.com>
2021-11-24 12:39:44 -0600

    Retrieve an Event Log

--- Get event log/Get-Logs.ps1
+++ Get event log/Get-Logs.ps1
@@ -1,3 +1,3 @@
 $Logname = (Read-host -Prompt "Type in the log name you wish to retreive")
-$newest = (Read-Host -Prompt "Enter the number of records you wish to see")
-Get-EventLog -LogName $Logname -Newest $newest
+$number = (Read-Host -Prompt "Enter the number of records you wish to see")
+Get-EventLog -LogName $Logname -Newest $number
