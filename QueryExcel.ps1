9e3cbbc26c2e02680b6c43999599256bda0d8b3d
Christian McGhee <chrs.mcghee@gmail.com>
2021-12-02 16:08:28 -0600

    Load Exel Workbook

--- QueryExcel.ps1
+++ QueryExcel.ps1
@@ -1,19 +1,19 @@
-
 $Value = Read-Host Enter value to check
 
-#Create excel connect
-$path =  "$env:temp\excelsampledata\financial.xlsx"
+#Create connection to Worksheet
+$path = "D:\Testfiles\financial.xlsx"
 
 $xl = New-Object -COM "Excel.Application"
-$xl.Visible = $true
+$xl.Visible = $false
 $wb = $xl.Workbooks.Open($path)
 #worksheet
 $ws = $wb.Sheets.Item(1)
 
 #look at cell ?
-for ($i = 1; $i -le 3; $i++) {
-  if ( $ws.Cells.Item($i, 1).Value -eq $value ) {
-    Write-host "Woot Woot"
-    break
-  }
+
+if ([bool]$ws.cells.find("$Value")) {
+    $foundit = "Yep $Value is there"
+    write-host $foundit
+       
 }
+    
