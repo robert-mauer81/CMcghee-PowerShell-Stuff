cc0c264fdbac3b933c11ea5475f10adc7c143e73
cmcghee92 <chrs.mcghee@gmail.com>
2022-12-05 14:59:54 -0800

    With Version 7

--- Eventlog.ps1
+++ Eventlog.ps1
@@ -1,3 +1,9 @@
+import-module Microsoft.PowerShell.Management
+
 $logname = (Read-host -Prompt "Enter Logname")
 $Newest = (Read-host -Prompt "Enter Number")
-Get-eventlog -logname $logname -Newest $Newest
+Get-EventLog -logname $logname -Newest $Newest
+
+#PS Version 7
+
+#Get-Winevent -logname $logname -MaxEvents $Newest
