ba150d1a2b6bafcadf90fcf554f48b46d3cc0e8c
Christian McGhee <chrs.mcghee@gmail.com>
2023-06-27 13:21:54 -0500

    Small edit

--- Get-LasteUserLogon.ps1
+++ Get-LasteUserLogon.ps1
@@ -1,5 +1,5 @@
 Write-Host "Enter the name of the Server you wish to query:  " -ForegroundColor White -BackgroundColor Red -NoNewline
 $Server = Read-Host
 GET-Wmiobject -Class Win32_NetworkLoginProfile -ComputerName $Server | Sort -Property LastLogon -Descending | Select-Object -Property * -First 2 | 
-Wher-object { $_.LastLogon -match "(\d{14})" } |
+Where-object { $_.LastLogon -match "(\d{14})" } |
 Foreach-Object { New-Object PSObject -Property @{ Name = $_.Name; LastLogon = [datetime]::ParseExact($matches[0], "yyyyMMddHHmmss", $null) } }
