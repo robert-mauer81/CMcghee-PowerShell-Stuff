ecaa1e69f953a7c27ddb3989898ebdebb9010ae5
Christian McGhee <chrs.mcghee@gmail.com>
2023-10-02 11:56:53 -0500

    Demo

--- Unlock Aduser/Unlock-Aduser.ps1
+++ Unlock Aduser/Unlock-Aduser.ps1
@@ -3,3 +3,6 @@ Param(
 [string]$user = (Read-Host -Prompt "Please enter the name of the user to unlock")
 )
 Get-Aduser -filter { Name -eq '$user' } | Unlock-ADAccount
+
+
+#This is a demonstration of an edit
